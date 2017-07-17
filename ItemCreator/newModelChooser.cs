36a938481c2330973fe585d3472b85b4d027dee7
Nils Berenbold <nils.berenbold@gmail.com>
2017-07-17 03:30:25 +0200

    - changed url to dol database

--- ItemCreator/newModelChooser.cs
+++ ItemCreator/newModelChooser.cs
@@ -297,7 +297,7 @@ namespace ItemCreator
 
             this.loading_panel.Visible = true;
 
-			string base_url = @"http://herald.uthgard.net/daoc/list.php?img=items|" + row.ID;
+			string base_url = @"http://www.dolserver.net/models/Models/items/" + row.ID + ".jpg";
 
             this.model_preview_picturebox.ImageLocation = base_url;
             this.current_loaded_image = row.ID;
